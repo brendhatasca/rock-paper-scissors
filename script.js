a18e5da35233a48b6ab6b0aa057595e9951285a5
brendhatasca <brendha.jakubiak@gmail.com>
2023-05-16 00:06:53 -0400

    Updated main game logic plus added a few styles

--- script.js
+++ script.js
@@ -1,57 +1,107 @@
+
+// Score keeper
 let playerScore = 0;
 let computerScore = 0;
+let roundWinner = '';
 
-
+// Generate pc choice
 function getComputerChoice() {
     let choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
-if ( (playerSelection == "paper" && computerSelection === "rock") ||
-(playerSelection == "scissors" && computerSelection == "paper") ||
-(playerSelection == "rock" && computerSelection == "scissors") ) {
-    playerScore += 1;
-    alert("You win! " + playerSelection + " beats " + computerSelection);
-} 
-else if ( (playerSelection == computerSelection) ) {
-    alert("It's a tie! You both chose " + playerSelection);
-} else {
-    computerScore += 1;
-    alert("You lost! " + computerSelection + " beats " + playerSelection);
-}}
+// Select each button
+const body = document.querySelector('body');
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
+// Add event listener for click
+rockBtn.addEventListener('click', () => handleClick('rock'));
+paperBtn.addEventListener('click', () => handleClick("paper"));
+scissorsBtn.addEventListener('click', () => handleClick("scissors"));
 
-function roundScore() {
-    alert('Scoreboard: Player ' + playerScore + ' x ' + computerScore + ' Computer');
+// Use info from click to play a round
+function handleClick(playerSelection) {
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    roundScore();
+    endGame();
 }
 
-function checkWinner() {
+// 
+function playRound(playerSelection, computerSelection) {
+    if ( (playerSelection == 'paper' && computerSelection === "rock") ||
+    (playerSelection == 'scissors' && computerSelection == "paper") ||
+    (playerSelection == 'rock' && computerSelection == "scissors") ) {
+        playerScore += 1;
+        roundWinner = 'Player';
+    } 
+    else if ( (playerSelection == computerSelection) ) {
+        roundWinner = 'Tie';
+    } else {
+        computerScore += 1;
+        roundWinner = 'Computer';
+}}
+
+// Display score
+function roundScore() {
+    let divScore = document.getElementById('round-result');
+    divScore.textContent = `Player ${playerScore} x ${computerScore} Computer`;
+    body.append(divScore);
+}
+
+// Finish game once score is 5
+function endGame() {
+    if ((playerScore == 5) || (computerScore == 5)) {
+    checkGameWinner();
+    disableButtons();
+    playAgain();
+    }
+}
+
+// Overall game winner
+function checkGameWinner() {
+    let displayWinner = document.getElementById('game-winner'); 
+
     if (playerScore > computerScore) {
-        return 'You beat the machine! The world isn\'t doomed after all.';
+        displayWinner.textContent = "You win! Humankind might not be doomed after all.";
+    } else {
+        displayWinner.textContent = "You lose! The Age of Ultron is coming.";
     }
-    else if (playerScore == computerScore) {
-        return 'Tie! Humans and robots are equally smart.'
-    }
-    else{
-        return 'The computer wins. The Age of Ultron is coming.';
-    }    
+    body.append(displayWinner);
+}
+
+// Disable buttons once ones score is 5
+function disableButtons() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+}
+
+// Refresh page button pops up
+function playAgain() {
+    const refreshBtn = document.createElement('button');
+    refreshBtn.textContent = "Play again";
+    refreshBtn.classList.add('refresh-page');
+    body.append(refreshBtn);
+    refreshBtn.addEventListener('click', () => window.location.reload())
 }
 
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Choose your player! Rock, paper or scissors?'); 
-        playerSelection = playerSelection.toLowerCase(); 
+    // for (let i = 0; i < 5; i++) {
+    //     let playerSelection = prompt('Choose your player! Rock, paper or scissors?'); 
+    //    playerSelection = playerSelection.toLowerCase(); 
         let computerSelection = getComputerChoice(); 
-        alert('The computer chose ' + computerSelection);
+    //    alert('The computer chose ' + computerSelection);
         playRound(playerSelection, computerSelection);
-        roundScore();
-}}
+    //    roundScore();
+};
 
-
-game();
-alert( checkWinner() );
+// Calls functions
+//game();
+//alert( checkWinner() );
 
 
     
